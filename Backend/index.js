7327183d763d03e6d0a316f7c8ff232db5b579f1
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-03-11 11:01:22 +0000

    started

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const ejs = require("ejs");
 
 const app = express();
+require("dotenv").config();
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("database connected successfully")
+}).catch(err => console.log(err))
 
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.render('welcome')
+})
+ 
+app.get("/login", (req, res) => {
+    res.render('login')
+})
+
+app.get("/register", (req, res) => {
+    res.render('register')
 })
 
 const PORT = process.env.PORT || 5000;
